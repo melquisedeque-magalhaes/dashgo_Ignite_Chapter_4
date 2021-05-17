53dbdedcc1ff7a4cdbd9d86d9c677a1c36b5a52c
Melqui <melqui.sodre15@gmail.com>
2021-05-17 12:00:22 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,68 @@
+import { Flex, Input, Button, Stack, FormLabel } from '@chakra-ui/react'
+
 export default function Home() {
   return (
-    <h1>DashGo</h1>
+    <Flex
+      align="center"
+      justify="center"
+      w="100vw"
+      height="100vh"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        padding={8}
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+
+          <div>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email" 
+              bg="gray.900"
+              focusBorderColor="pink.500"
+              variant="filled"
+              _hover={{
+                bg: "gray.900"
+              }}
+              size="lg"
+            />
+          </div>
+        
+          <div>
+            <FormLabel htmlFor="password">E-mail</FormLabel>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              bg="gray.900"
+              focusBorderColor="pink.500"
+              variant="filled"
+              _hover={{
+                bg: "gray.900"
+              }}
+              size="lg"
+            />
+          </div>
+          
+        </Stack>
+
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
+
+      </Flex>
+    </Flex>
   )
 }
